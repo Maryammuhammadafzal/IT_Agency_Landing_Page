c68535f07e3fc414c4e1397a8bf6e891ca29dd69
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-12 11:03:31 +0500

    created Responsive

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -5,22 +5,22 @@ const HeroPage = () => {
     <div className="home mb-0 w-full font-[Darker Grotesque] h-auto flex justify-center items-start pt-6">
       <div className="w-full p-2 sm:w-[90%] lg:w-[85%]  h-auto flex md:flex-row flex-col justify-center items-center gap-3">
         <div className="left md:w-[50%] w-full h-auto space-y-5">
-          <h1 className="font-bold font-[Montserrat] 2xl:text-7xl 2xl:leading-20 xl:text-6xl lg:text-5xl md:text-5xl min-[768px]:text-4xl text-6xl xl:leading-16 lg:leading-14 max-sm:text-5xl max-xs:text-4xl">
+          <h1 className="font-bold font-[Montserrat] 2xl:text-7xl 2xl:leading-20 xl:text-6xl lg:text-5xl md:text-4xl min-[768px]:text-4xl text-6xl xl:leading-16 lg:leading-14 max-sm:text-5xl max-xs:text-4xl">
             Your Creative Agency for the Digital Age
           </h1>
-          <p className="font-medium md:text-base text-base max-xs:text-xs max-sm:text-sm">
+          <p className="font-medium md:text-sm lg:text-base leading-4 text-base max-xs:text-xs max-sm:text-sm">
             At WebCrafters, we specialize in crafting innovative digital
             solutions that transform ideas into impactful online experiences.
             Whether you're a startup, small business, or established enterprise,
             our team is dedicated to elevating your brand in the digital
             landscape.
           </p>
-          <div className="leading flex gap-12 max-sm:gap-5 py-3 max-xs:py-0">
+          <div className="flex gap-12 max-sm:gap-5 py-3 max-xs:py-0">
             <div className="flex flex-col font-semibold">
-              <h2 className="text-5xl md:text-7xl max-xs:text-4xl font-extrabold text-orange-500">
+              <h2 className="text-4xl  md:text-5xl lg:text-7xl max-xs:text-4xl font-extrabold text-orange-500">
                 +500
               </h2>
-              <p className="text-2xl md:text-xl max-xs:text-base font-semibold pl-2">
+              <p className="text-2xl leading-none md:text-xl max-xs:text-base font-semibold pl-2">
                 Website Dilevered
               </p>
             </div>
@@ -28,17 +28,14 @@ const HeroPage = () => {
               /
             </div>
             <div className="flex flex-col font-semibold">
-              <h2 className="text-5xl md:text-7xl max-xs:text-4xl font-extrabold text-orange-500">
+              <h2 className="text-4xl md:text-5xl  lg:text-7xl max-xs:text-4xl font-extrabold text-orange-500">
                 +10
               </h2>
-              <p className="text-2xl md:text-xl max-xs:text-base font-semibold pl-2">
+              <p className="text-2xl md:text-xl leading-none max-xs:text-base font-semibold pl-2">
                 Year Of Expertise
               </p>
             </div>
           </div>
-          {/* <Button className=" max-sm:px-4 max-sm:py-4 py-6 px-8 rounded-lg text-white bg-[#007BFF] font-semibold text-[16px] max-sm:text-sm hover:bg-[#1B1F3B] hover:scale-x-90">
-            Get Started Now
-          </Button> */}
         </div>
         <div className="right md:w-[50%] w-full h-auto flex justify-center items-center">
           <img src={HeroImage.src} title="Digital Agency" alt="Digital Agency" />
