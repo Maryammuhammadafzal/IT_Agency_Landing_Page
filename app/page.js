cd50bbc448b7d3c44b7be7a937bb021782cfa760
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-12 11:37:14 +0500

    Creating text maquee

--- app/page.js
+++ app/page.js
@@ -9,10 +9,12 @@ import '@fontsource/darker-grotesque/700.css';
 import '@fontsource/montserrat/800.css';
 import Button from '@mui/material/Button';
 import HeroPage from './Hero/Page';
+import TextSlider from '@/component/TextSlider';
 export default function Home() {
   return (
     <div className="main w-full h-auto bg-white overflow-hidden">
  <HeroPage/>
+ <TextSlider/>
     </div>
   );
 }
---
+++ component/TextSlider.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TextSlider = () => {
+  return (
+    <div className="h-auto w-full py-10 overflow-x-hidden">
+        <div className="w-full whitespace-nowrap py-3 flex gap-6 justify-center items-center ">
+      <span className="gradient-title text-9xl font-bold">DIGITAL WALLET</span>
+      <span className="gradient-title text-9xl font-bold">DIGITAL WALLET</span>
+      <span className="gradient-title text-9xl font-bold">DIGITAL WALLET</span>
+    </div>
+    </div>
+  );
+};
+
+export default TextSlider;
