c6e5af6a0aa747d28ea6b2dd95c970fa7bdd48e9
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-12 14:42:12 +0500

    Created

--- app/Services/Page.jsx
+++ app/Services/Page.jsx
@@ -1,24 +1,29 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import CallMadeIcon from '@mui/icons-material/CallMade';
 
 const ServicesPage = () => {
 
-   let about_data = [
+   let services_data = [
     {
-      title : "Top-Tier Security",
-      description: "We understand that every business is unique. Our team works closely with you to develop customized strategies that align with your goals and resonate with your target audience."
+      title : "web development",
+      description: "Build responsive, high-performance websites that engage and convert visitors. Our developers leverage the latest technologies to deliver exceptional user experiences."
     },
     {
-      title : "Cutting-Edge Design",
-      description: "Stand out with visually stunning and user-centric designs. From websites to social media campaigns, our creative team ensures your brand looks its best across all platforms."
+      title : "Graphic Design",
+      description: "Enhance your visual identity with stunning logos, banners, and promotional materials that capture your brand’s essence."
     },
     {
-      title : "Full Services",
-      description: "From web development and graphic design to SEO and digital marketing, we offer a full suite of services to meet all your digital needs."
+      title : "SEO & Content Marketing",
+      description: "Boost your online visibility with strategic SEO and content that ranks. From keyword optimization to blog writing, we help drive traffic and engagement."
     },
     {
-      title : "Data-Driven Results",
-      description: "Make informed decisions with detailed analytics and performance reports. We provide insights to help you optimize your digital presence and achieve measurable growth."
+      title : "Social Media Management",
+      description: "Connect with your audience on platforms that matter. We create and manage compelling social media campaigns to grow your brand presence."
+    },
+    {
+      title : "Brand Strategy",
+      description: "Develop a strong and cohesive brand strategy that sets you apart from competitors and fosters customer loyalty."
     },
   ]
 
@@ -31,17 +36,23 @@ const ServicesPage = () => {
             <span className="text-orange-500">We Offer</span>
           </h1>
           <div className="flex justify-start w-full h-auto items-start py-3">
-            <div className="flex flex-wrap justify-between items-start gap-4 w-full h-auto">
+            <div className="flex flex-wrap justify-center items-start gap-10 w-full h-auto">
            
-           <div className="card w-auto h-auto flex justify-center items-center p-3 border-2  border-orange-500 rounded-lg">
-            <div className=' w-full h-full flex flex-col gap-3 justify-center items-center text-center'>
-              <h2 className="gradient-number min-[350px]:text-8xl text-7xl xl:text-9xl">Web Development</h2>
-               <p className="text-black/75 xl:leading-5 sm:leading-4 leading-3 pr-2 font-medium sm:text-sm text-xs md:text-base">
-                   Build responsive, high-performance websites that engage and convert visitors. Our developers leverage the latest technologies to deliver exceptional user experiences.
+         {services_data && services_data.map(({title , description}, index) => (
+            <div key={index} className="card w-[340px] h-[330px] flex justify-center items-center p-3 border-2  border-orange-500 rounded-3xl">
+            <div className=' w-full h-full flex flex-col gap-4 justify-center items-center text-center'>
+              <h2 className="text-[40px] font-bold">{title}</h2>
+               <p className="text-black/75 xl:leading-5 sm:leading-4 leading-3 pr-2 font-medium sm:text-base text-xs md:text-xl">
+                  {description}
                   </p>
-                  <Button></Button>
+                  <button className='button relative w-[170px] text-white my-3 font-bold capitalize text-xl h-10 flex justify-between items-center gap-3 pr-1 pl-5 py-3 bg-orange-500 rounded-full'>
+                    <span id='text'>view more</span> <div className="arrow-btn w-8 h-8 rounded-full flex justify-center items-center bg-white">
+                      <CallMadeIcon fontSize='small' className='text-orange-500'/>
+                    </div>
+                  </button>
             </div>
            </div>
+         ))}
             </div>
           </div>
         </div>
