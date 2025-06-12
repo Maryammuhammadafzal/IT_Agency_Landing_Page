6c506708ac6b3f8792ed66ffc1683af17000f8c0
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-12 11:47:53 +0500

    Created

--- component/TextSlider.jsx
+++ component/TextSlider.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const TextSlider = () => {
   return (
-    <div className="h-auto w-full py-10 overflow-x-hidden">
-        <div className="w-full whitespace-nowrap py-3 flex gap-6 justify-center items-center ">
-      <span className="gradient-title text-9xl font-bold">DIGITAL WALLET</span>
-      <span className="gradient-title text-9xl font-bold">DIGITAL WALLET</span>
-      <span className="gradient-title text-9xl font-bold">DIGITAL WALLET</span>
-    </div>
+    <div className="h-auto w-full py-3 lg:py-10 overflow-x-hidden">
+        <Marquee autoFill={true} className="w-full whitespace-nowrap flex gap-10 justify-center items-center ">
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+      <span className="gradient-title text-5xl sm:text-7xl lg:text-9xl font-bold px-6">DIGITAL WALLET</span>
+    </Marquee>
     </div>
   );
 };
